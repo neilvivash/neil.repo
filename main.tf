615ffbde9925c559e3512818e0bb492060a017f5
neilvivash <neilvivash4@gmail.com>
2022-08-10 03:39:31 -0400

    first commit to devterra

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 terraform {
   required_providers {
     aws = {
@@ -24,3 +25,18 @@ resource "aws_instance" "app_server" {
 
   }
 }
+=======
+resource "aws_instance" "my-machine" {
+  # Creates four identical aws ec2 instances
+  count = 4     
+  
+  # All four instances will have the same ami and instance_type
+  ami = lookup(var.ec2_ami,var.region) 
+  instance_type = var.instance_type # 
+  tags = {
+    # The count.index allows you to launch a resource 
+    # starting with the distinct index number 0 and corresponding to this instance.
+    Name = "my-machine-${count.index}"
+  }
+}
+>>>>>>> 6daaaeb (first push to new set up)
---
+++ vars.tf
@@ -0,0 +1,23 @@
+# Creating a Variable for ami of type map
+
+
+variable "ec2_ami" {
+  type = map
+
+  default = {
+    eu-west-2 = "ami-0bb712bfc5b5ce8f9"
+    
+  }
+}
+
+# Creating a Variable for region
+
+# variable "region" {
+#  default = eu-west-2
+#}
+
+
+# Creating a Variable for instance_type
+variable "instance_type" {    
+  type = string
+}
