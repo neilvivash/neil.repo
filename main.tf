a8b35c2286655c9732650458a62a9028c0aa091d
neilvivash <61456528+neilvivash@users.noreply.github.com>
2022-08-10 11:43:32 +0100

    Update main.tf
    removed var.region from ami lookup "ami = lookup(var.ec2_ami,var.region"

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "my-machine" {
   count = 4     
   
   # All four instances will have the same ami and instance_type
-  ami = lookup(var.ec2_ami,var.region) 
+  ami = lookup(var.ec2_ami) 
   instance_type = var.instance_type # 
   tags = {
     # The count.index allows you to launch a resource 
